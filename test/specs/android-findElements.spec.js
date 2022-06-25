bb0aceca0bd0c6d905f59dc16a8afcf1609b1edb
lucas <lucashdoa@gmail.com>
2022-06-25 15:20:16 -0300

    first commit

---
+++ test/specs/android-findElements.spec.js
@@ -0,0 +1,11 @@
+describe('Android Elements Test', () => {
+    it('Find element by accessibility id', async () => {
+        // find element by accessibility id
+        const appOption = await $('~App')
+        // click on element
+        await appOption.click()
+        // assertion
+        const actionBar = await $('~Action Bar')
+        await expect(actionBar).toBeExisting()
+    })
+})
---
+++ test/specs/sample.js
@@ -0,0 +1,5 @@
+describe('Sample', () => {
+    it('Should do nothing', () => {
+        
+    })
+})
